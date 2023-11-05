be3a4100f02538661e1fa820a6a7deed8dcf295e
Praveen <rpraveen.engineer@gmail.com>
2023-11-05 17:21:04 +0530

    Added mouse cursor

---
+++ scenes/ui/mouse_cursor/mosue_cursor.gd
@@ -0,0 +1,15 @@
+extends CanvasLayer
+
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
+
+
+
+func _process(_delta: float):
+	sprite_2d.global_position = sprite_2d.get_global_mouse_position()
+	if Input.is_action_just_pressed("click"):
+		animation_player.play("click")
+		
