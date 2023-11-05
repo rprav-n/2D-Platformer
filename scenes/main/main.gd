e782d3d61eea719209d624e198ac5cd9a579846f
Praveen <rpraveen.engineer@gmail.com>
2023-11-05 13:39:02 +0530

    Added main menu

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -4,4 +4,3 @@ extends Node
 
 func _ready():
 	RenderingServer.set_default_clear_color(background_color)
-	LevelManager.change_level(0)
---
+++ scenes/ui/main_menu/main_menu.gd
@@ -0,0 +1,21 @@
+class_name MainMenu
+
+extends CanvasLayer
+
+@onready var play_button: Button = %PlayButton
+@onready var option_button: Button = %OptionButton
+@onready var quit_button: Button = %QuitButton
+
+
+func _ready():
+	play_button.pressed.connect(_on_play_button_pressed)
+	quit_button.pressed.connect(_on_quit_button_pressed)
+
+
+func _on_play_button_pressed():
+	LevelManager.change_level(0)
+	
+	
+func _on_quit_button_pressed():
+	await get_tree().create_timer(0.3).timeout
+	get_tree().quit()
