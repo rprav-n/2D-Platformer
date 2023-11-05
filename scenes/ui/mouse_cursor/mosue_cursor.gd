51f01a9474b39308032e5ef7c7877143ac31033d
Praveen <rpraveen.engineer@gmail.com>
2023-11-05 17:29:00 +0530

    Change display widow size by 1.5 times

--- scenes/ui/mouse_cursor/mosue_cursor.gd
+++ scenes/ui/mouse_cursor/mosue_cursor.gd
@@ -7,7 +7,6 @@ func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
 
 
-
 func _process(_delta: float):
 	sprite_2d.global_position = sprite_2d.get_global_mouse_position()
 	if Input.is_action_just_pressed("click"):
